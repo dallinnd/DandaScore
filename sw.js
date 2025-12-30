d31a5e4538e12899b04779a6f505082ed7572d67
dallinnd <156145316+dallinnd@users.noreply.github.com>
2025-12-30 00:48:00 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,10 @@
-const CACHE_NAME = 'panda-v2';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  'https://cdn.tailwindcss.com'
-];
+const CACHE_NAME = 'panda-v12';
+const ASSETS = ['./', './index.html', './style.css', './app.js', 'https://cdn.tailwindcss.com'];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
 });
